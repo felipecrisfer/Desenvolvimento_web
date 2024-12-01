399337c59e767e13ae5cab843973db5d15eaeb54
Felipe Cristian Fernandes <114928609+felipecrisfer@users.noreply.github.com>
2024-12-01 15:17:11 -0300

    Update contato.js
    adicionado imagem do insta e do linkedin entre outras alterações

--- Vue.JS/js/contato.js
+++ Vue.JS/js/contato.js
@@ -1,6 +1,6 @@
 const PaginaContato = {
     template:
-     `
+        `
     <!-- Cabeçalho com fundo em degradê -->
     <header>
         <h1>Contatos dos Responsáveis</h1>
@@ -24,8 +24,9 @@ const PaginaContato = {
                         <h5 class="card-title">David Bryan</h5>
                         <p class="card-text">Coordenador de Projetos</p>
                         <p><strong>Telefone:</strong> (11) 98765-4321</p>
-                        <p><strong>LinkedIn:</strong> 
-                            <a href="https:" target="_blank">David Bryan no LinkedIn</a>
+                        <p class="link">
+                            <a href="https:" target="_blank"><img src="Imagens/linkedin.png" style="height: 40px; width: 40px;"></a>
+                            <a href="https:" target="_blank"><img src="Imagens/insta.png" style="height: 40px; width: 40px;"></a>
                         </p>
                     </div>
                 </div>
@@ -35,14 +36,15 @@ const PaginaContato = {
             <div class="col-md-4 mb-4">
                 <div class="card">
                     <img src="Imagens/Felipe.png" class="card-img-top" alt="Responsável 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Felipe Fernandes</h5>
-                        <p class="card-text">Gerente de Marketing</p>
-                        <p><strong>Telefone:</strong> (21) 91234-5678</p>
-                        <p><strong>LinkedIn:</strong> 
-                            <a href="https:" target="_blank">Felipe Fernandes no LinkedIn</a>
-                        </p>
-                    </div>
+            <div class="card-body">
+                <h5 class="card-title">Felipe Fernandes</h5>
+                <p class="card-text">Gerente de Marketing</p>
+                <p><strong>Telefone:</strong> (21) 91234-5678</p>
+                <p>
+                    <a href="https://www.linkedin.com/in/felipe-fernandes-722590260/" target="_blank"><img src="Imagens/linkedin.png" style="height: 40px; width: 40px;"></a>
+                    <a href="https://www.instagram.com/felipecrisfer/" target="_blank"><img src="Imagens/insta.png" style="height: 40px; width: 40px;"></a>
+                </p>
+                </div>
                 </div>
             </div>
 
@@ -54,8 +56,9 @@ const PaginaContato = {
                         <h5 class="card-title">Robertha Rezende</h5>
                         <p class="card-text">Diretora de Comunicação</p>
                         <p><strong>Telefone:</strong> (31) 98765-4321</p>
-                        <p><strong>LinkedIn:</strong> 
-                            <a href="https:" target="_blank">Robertha Rezende no LinkedIn</a>
+                        <p>
+                        <a href="https:" target="_blank"><img src="Imagens/linkedin.png" style="height: 40px; width: 40px;"></a>
+                        <a href="https:" target="_blank"><img src="Imagens/insta.png" style="height: 40px; width: 40px;"></a>
                         </p>
                     </div>
                 </div>
@@ -71,8 +74,9 @@ const PaginaContato = {
                         <h5 class="card-title">Victor Araújo</h5>
                         <p class="card-text">Supervisor de TI</p>
                         <p><strong>Telefone:</strong> (41) 91234-6789</p>
-                        <p><strong>LinkedIn:</strong> 
-                            <a href="https:" target="_blank">Victor Araújo no LinkedIn</a>
+                        <p>
+                        <a href="https:" target="_blank"><img src="Imagens/linkedin.png" style="height: 40px; width: 40px;"></a>
+                        <a href="https://www.instagram.com/victor.dll/" target="_blank"><img src="Imagens/insta.png" style="height: 40px; width: 40px;"></a>
                         </p>
                     </div>
                 </div>
